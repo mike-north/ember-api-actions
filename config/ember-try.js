148c34dec1daf931c0a4a7e61171f4771c682033
Mike North <michael.l.north@gmail.com>
2015-07-05 19:16:28 -0700

    Fix tests

--- config/ember-try.js
+++ config/ember-try.js
@@ -5,6 +5,42 @@ module.exports = {
       dependencies: { }
     },
     {
+      name: 'ember-1.10',
+      dependencies: {
+        'ember': '~1.10.0'
+      },
+      resolutions: {
+        'ember': '~1.10.0'
+      }
+    },
+    {
+      name: 'ember-1.11',
+      dependencies: {
+        'ember': '~1.11.0'
+      },
+      resolutions: {
+        'ember': '~1.11.0'
+      }
+    },
+    {
+      name: 'ember-1.12',
+      dependencies: {
+        'ember': '~1.12.0'
+      },
+      resolutions: {
+        'ember': '~1.12.0'
+      }
+    },
+    {
+      name: 'ember-release',
+      dependencies: {
+        'ember': 'components/ember#release'
+      },
+      resolutions: {
+        'ember': 'release'
+      }
+    },
+    {
       name: 'ember-release',
       dependencies: {
         'ember': 'components/ember#release'
@@ -16,19 +52,23 @@ module.exports = {
     {
       name: 'ember-beta',
       dependencies: {
-        'ember': 'components/ember#beta'
+        'ember': 'components/ember#beta',
+        'ember-data': 'components/ember-data#canary'
       },
       resolutions: {
-        'ember': 'beta'
+        'ember': 'beta',
+        'ember-data': 'canary'
       }
     },
     {
       name: 'ember-canary',
       dependencies: {
-        'ember': 'components/ember#canary'
+        'ember': 'components/ember#canary',
+        'ember-data': 'components/ember-data#canary'
       },
       resolutions: {
-        'ember': 'canary'
+        'ember': 'canary',
+        'ember-data': 'canary'
       }
     }
   ]
--- tests/dummy/app/routes/index.js
+++ tests/dummy/app/routes/index.js
@@ -3,44 +3,57 @@ import Pretender from 'pretender';
 
 export default Ember.Route.extend({
   model() {
-    return Ember.A(this.store.pushMany('fruit', [
-      {id: 1, name: 'apple'},
-      {id: 2, name: 'pear'},
-      {id: 3, name: 'orange'},
-      {id: 4, name: 'grape'},
-    ]));
-  },
+      return Ember.A(this.store.pushMany('fruit', [{
+        id: 1,
+        name: 'apple'
+      }, {
+        id: 2,
+        name: 'pear'
+      }, {
+        id: 3,
+        name: 'orange'
+      }, {
+        id: 4,
+        name: 'grape'
+      }, ]));
+      // return this.get('store').findAll('fruit');
+    },
 
-  activate() {
-    this._super(...arguments);
-    if (!Ember.testing) {
-      this._setupPretender();
+    beforeModel() {
+      this._super(...arguments);
+      if (!Ember.testing) {
+        this._setupPretender();
+      }
+    },
+    deactivate() {
+      this._super(...arguments);
+    },
+    _setupPretender() {
+      let server = new Pretender();
+      // server.get('/fruits', request => {
+      //   return [200, {}, JSON.stringify({
+      //     fruits:
+      //   })];
+      // });
+      server.put('/fruits/:id/doRipen', request => {
+        let controller = this.get('controller');
+        controller.get('requests').addObject({
+          url: request.url,
+          data: JSON.parse(request.requestBody)
+        });
+        return [200, {}, '{"status": "ok"}'];
+      });
+      server.put('/fruits/ripenEverything', request => {
+        let controller = this.get('controller');
+        controller.get('requests').addObject({
+          url: request.url,
+          data: JSON.parse(request.requestBody)
+        });
+        return [200, {}, '{"status": "ok"}'];
+      });
+      this.set('server', server);
+    },
+    _teardownPretender() {
+      this.get('server').shutdown();
     }
-  },
-  deactivate() {
-    this._super(...arguments);
-  },
-  _setupPretender() {
-    let server = new Pretender();
-    server.put('/fruits/:id/doRipen', request => {
-      let controller = this.get('controller');
-      controller.get('requests').addObject({
-        url: request.url,
-        data: JSON.parse(request.requestBody)
-      });
-      return [200, {}, '{"status": "ok"}'];
-    });
-    server.put('/fruits/ripenEverything', request => {
-      let controller = this.get('controller');
-      controller.get('requests').addObject({
-        url: request.url,
-        data: JSON.parse(request.requestBody)
-      });
-      return [200, {}, '{"status": "ok"}'];
-    });
-    this.set('server', server);
-  },
-  _teardownPretender() {
-    this.get('server').shutdown();
-  }
 });

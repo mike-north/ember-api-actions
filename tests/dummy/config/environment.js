049bd6f4f60ca9ccfc8d570461be9b339da60b12
Mike North <michael.l.north@gmail.com>
2015-07-05 17:59:22 -0700

    gh-pages fixes

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -39,7 +39,7 @@ module.exports = function(environment) {
     ENV.APP.rootElement = '#ember-testing';
   }
 
-  if (environment === 'production') {
+  if (environment === 'development') {
     ENV.locationType = 'hash';
     ENV.baseURL = 'ember-api-actions/';
   }

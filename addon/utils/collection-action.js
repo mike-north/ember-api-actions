a8453dd23682d2cbdf1ad45946ad93b67cb46612
Devonte <devo.tox.89@gmail.com>
2017-06-30 12:22:51 +0100

    Update collection-action.js

--- addon/utils/collection-action.js
+++ addon/utils/collection-action.js
@@ -4,12 +4,16 @@ import { buildOperationUrl } from './build-url';
 const { merge } = Ember;
 
 export default function instanceOp(options) {
-  return function(payload) {
+  return function(payload, addToPath) {
     let modelName = this.constructor.modelName || this.constructor.typeKey;
     let requestType = (options.type || 'PUT').toUpperCase();
     let urlType = options.urlType || requestType;
     let adapter = this.store.adapterFor(modelName);
-    let fullUrl = buildOperationUrl(this, options.path, urlType, false);
+    let path = options.path;
+    if(addToPath) {
+      path = `${path}/${addToPath}`
+    }
+    let fullUrl = buildOperationUrl(this, path, urlType, false);
     return adapter.ajax(fullUrl, requestType, merge(options.ajaxOptions || {}, { data: payload }));
   };
 }

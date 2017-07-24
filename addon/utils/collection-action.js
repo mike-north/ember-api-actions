0cee335a62da130696187ae2e631a7645d2ff18b
Stefano Valicchia <stefano.valicchia@gmail.com>
2017-07-24 14:33:45 +0200

    Added option to extract a field name from response

--- addon/utils/collection-action.js
+++ addon/utils/collection-action.js
@@ -4,12 +4,23 @@ import { buildOperationUrl } from './build-url';
 const { merge } = Ember;
 
 export default function instanceOp(options) {
-  return function(payload) {
-    let modelName = this.constructor.modelName || this.constructor.typeKey;
-    let requestType = (options.type || 'PUT').toUpperCase();
-    let urlType = options.urlType || requestType;
-    let adapter = this.store.adapterFor(modelName);
-    let fullUrl = buildOperationUrl(this, options.path, urlType, false);
-    return adapter.ajax(fullUrl, requestType, merge(options.ajaxOptions || {}, { data: payload }));
-  };
+    return function(payload) {
+        let modelName = this.constructor.modelName || this.constructor.typeKey;
+        let requestType = (options.type || 'PUT').toUpperCase();
+        let urlType = options.urlType || requestType;
+        let adapter = this.store.adapterFor(modelName);
+        let fullUrl = buildOperationUrl(this, options.path, urlType, false);
+        let extract = options.extract;
+        return adapter.ajax(fullUrl, requestType, merge(options.ajaxOptions || {}, { data: payload })).then(function(response) {
+            if (extract) {
+                if (response && response[extract] !== null) {
+                    return Ember.RSVP.response(response[extract]);
+                } else {
+                    return Ember.RSVP.reject(new Error('Response malformed'));
+                }
+            } else {
+                return Ember.RSVP.response(response);
+            }
+        });
+    };
 }

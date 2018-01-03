9f1bc3f0a7762659ae0a612c0b0316e426992ad1
Michael North <michael.l.north@gmail.com>
2018-01-02 18:59:14 -0800

    Linting fixes

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// eslint-disable-next-line
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'dummy',
--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line
 module.exports = {
   browsers: ['ie 9', 'last 1 Chrome versions', 'last 1 Firefox versions', 'last 1 Safari versions']
 };

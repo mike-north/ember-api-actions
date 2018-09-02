ce539bfd93dde61477de9eb517afbae6cf425255
Mike North <michael.l.north@gmail.com>
2018-09-02 00:58:18 -0700

    fix: linting

--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -1,10 +1,7 @@
+/* eslint-env node */
 'use strict';
 
-const browsers = [
-  'last 1 Chrome versions',
-  'last 1 Firefox versions',
-  'last 1 Safari versions'
-];
+const browsers = ['last 1 Chrome versions', 'last 1 Firefox versions', 'last 1 Safari versions'];
 
 const isCI = !!process.env.CI;
 const isProduction = process.env.EMBER_ENV === 'production';

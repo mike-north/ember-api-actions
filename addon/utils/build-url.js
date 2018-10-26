f4686219c880e23bfd878d77996ca0d7f2167aa1
Isaac Ezer <isaac.ezer@gmail.com>
2018-10-26 13:53:05 +0200

    feature: Allow empty paths

--- addon/utils/build-url.js
+++ addon/utils/build-url.js
@@ -1,13 +1,14 @@
-import { assert } from '@ember/debug';
-
 export function buildOperationUrl(record, opPath, urlType, instance = true) {
-  assert('You must provide a path for instanceOp', opPath);
   let modelName = record.constructor.modelName || record.constructor.typeKey;
   let adapter = record.store.adapterFor(modelName);
   let path = opPath;
   let snapshot = record._createSnapshot();
   let baseUrl = adapter.buildURL(modelName, instance ? record.get('id') : null, snapshot, urlType);
 
+  if (!path) {
+    return baseUrl;
+  }
+  
   if (baseUrl.charAt(baseUrl.length - 1) === '/') {
     return `${baseUrl}${path}`;
   } else {

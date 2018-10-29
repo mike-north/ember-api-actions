7525eb8352d99c08c8f34787b6a3c0ed6d777540
Isaac Ezer <isaac.ezer@gmail.com>
2018-10-29 13:54:22 +0100

    chore: remove serializeAndPush util
    Will extract as a separate PR

--- addon/index.js
+++ addon/index.js
@@ -1,8 +1,7 @@
 import memberAction from './utils/member-action';
 import collectionAction from './utils/collection-action';
-import serializeAndPush from './utils/serialize-and-push';
 
 export const classOp = collectionAction;
 export const instanceOp = memberAction;
 
-export { collectionAction, memberAction, serializeAndPush };
+export { collectionAction, memberAction };

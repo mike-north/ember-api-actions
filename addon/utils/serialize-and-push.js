638dbbeb7b0cd8cd229f2395adff4cd9787af8fe
Isaac Ezer <isaac.ezer@gmail.com>
2018-10-29 12:12:53 +0100

    chore: require jsonapi document in serializeAndPush

--- addon/utils/serialize-and-push.js
+++ addon/utils/serialize-and-push.js
@@ -1,7 +1,14 @@
 import { isArray } from '@ember/array';
 import { _getModelClass, _getModelName, _getStoreFromRecord } from './build-url';
 
-export default function serializeAndPush(options, response) {  
+export default function serializeAndPush(options, response) {
+  const isJsonApi = response.jsonapi && response.jsonapi.version;
+  if (!isJsonApi) {
+    // eslint-disable-next-line no-console
+    console.warn('serializeAndPush may only be used with a JSON API document. Ignoring response. Document must have a mandatory JSON API object. See https://jsonapi.org/format/#document-jsonapi-object.');
+    return response;
+  }
+  
   const recordClass = _getModelClass(this);
   const modelName = _getModelName(recordClass);
   const store = _getStoreFromRecord(this);

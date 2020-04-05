768641fb0c06b2a92e59c1a1b666c6b0052455de
Alex LaFroscia <alex@lafroscia.com>
2020-04-05 14:20:38 -0400

    feat: add decorator for `collectionAction`

--- addon/decorators.ts
+++ addon/decorators.ts
@@ -1,4 +1,13 @@
+import collectionOp, { CollectionOperationOptions } from './utils/collection-action';
 import instanceOp, { InstanceOperationOptions } from './utils/member-action';
+
+export function collectionAction<IN = any, OUT = any>(options: CollectionOperationOptions<IN, OUT>) {
+  return function createCollectionActionDescriptor(target: any, propertyName: string): any {
+    return {
+      value: collectionOp(options)
+    };
+  }
+}
 
 export function memberAction<IN = any, OUT = any>(options: InstanceOperationOptions<IN, OUT>) {
   return function createMemberActionDescriptor(target: any, propertyName: string): any {

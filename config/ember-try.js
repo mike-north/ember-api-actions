a98653f74fd7a584fc89498df7d90b301c8933d4
Alex LaFroscia <alex@lafroscia.com>
2020-04-16 10:32:24 -0400

    chore: Drop support for Ember v3.19 and below
    BREAKING CHANGE: We are no longer actively testing against Ember versions lower than 3.20

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,30 +7,6 @@ module.exports = async function() {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-2.16',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^0.5.1',
-            'ember-source': '~2.16.0'
-          }
-        }
-      },
-      {
-        name: 'ember-lts-2.18',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^0.5.1',
-            'ember-source': '~2.18.0'
-          }
-        }
-      },
-      {
         name: 'ember-lts-3.20',
         npm: {
           devDependencies: {
